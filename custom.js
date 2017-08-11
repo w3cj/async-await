314dd538829526d3b8cf25d666d0ae5c500a4cc1
CJ R <cj@null.computer>
2017-08-11 08:32:16 -0600

    :link: update link

--- custom.js
+++ custom.js
@@ -1,3 +1,3 @@
 var link = document.createElement('div');
-link.innerHTML = '<style>.reveal { font-size: 1.7em !important }</style><div style="position:fixed;bottom:15px;left:20px" class="dark-bg"><a style="font-family: \'Open Sans\', sans-serif;font-size: 30px;font-weight: normal;color: #e7ad52;text-decoration: none;display: block;" target="_blank" href="https://git.io/async-await">git.io/async-await</a></div>';
+link.innerHTML = '<style>.reveal { font-size: 1.7em !important }</style><div style="position:fixed;bottom:15px;left:20px" class="dark-bg"><a style="font-family: \'Open Sans\', sans-serif;font-size: 30px;font-weight: normal;color: #e7ad52;text-decoration: none;display: block;" target="_blank" href="https://git.io/dd-async-await">git.io/dd-async-await</a></div>';
 document.body.appendChild(link);
